fd8083119f8eccdaabcbb780d6d3274af78d8320
Dnsmpr <104632579+Dnsmpr@users.noreply.github.com>
2023-12-30 15:42:06 +0100

    Add solution for problem 07.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  if(f == 32) {
+    return 0;
+  }
+  return parseFloat((5/9*(f - 32)).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  if(c == 0) {
+    return 32;
+  }
+  return parseFloat(((c * 9/5 + 32)).toFixed(1));
 };
 
 // Do not edit below this line
