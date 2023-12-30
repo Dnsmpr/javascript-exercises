46fd57ed09701f1b34900e8d9dd0572f4ef0e416
Dnsmpr <104632579+Dnsmpr@users.noreply.github.com>
2023-12-30 14:48:17 +0100

    Add solution for problem 03.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let charArray = word.split("");
+    for(let i = 0; i < word.length / 2; i++) {
+        temp = word[i];
+        charArray[i] = charArray[word.length - i - 1];
+        charArray[word.length - i - 1] = temp;
+    }
+    return charArray.join("");
 };
 
 // Do not edit below this line
