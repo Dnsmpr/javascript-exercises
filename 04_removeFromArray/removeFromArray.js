ee57604475ed7561330239de8c5a0a63c2bef9e1
Dnsmpr <104632579+Dnsmpr@users.noreply.github.com>
2023-12-30 15:11:51 +0100

    Add solution to problem 04.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(list, ...Args) {
+    for(const arg of Args) {
+        for(let i = 0; i < list.length; i++) {
+            if(list[i] === arg) {
+                list.splice(i, 1);
+            }
+        }
+    }
+    return list;
 };
 
+// xxx0
 // Do not edit below this line
 module.exports = removeFromArray;
