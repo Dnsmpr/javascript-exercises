8d5adcb1db2832f9ef3b2c540df3946cc64c7aca
Dnsmpr <104632579+Dnsmpr@users.noreply.github.com>
2023-12-30 15:53:09 +0100

    Add solution to problem 08.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(list) {
+	let sum = 0;
+  for(const item of list) {
+    sum += item;
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(list) {
+  let product = 1;
+  for(const item of list) {
+    product *= item;
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	if(number === 0 || number === 1) {
+    return 1;
+  }
+  let fact = 1;
+  for(let i = number; i > 1; i--) {
+    fact *= i;
+  }
+  return fact;
 };
 
 // Do not edit below this line
