400a67cd6de3dfc793182bc16ae8d5240afef60e
Dnsmpr <104632579+Dnsmpr@users.noreply.github.com>
2023-12-30 16:23:36 +0100

    Add solution for problem 09.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    let invalidChar = [" ", "!", ".", ","];
 
+    newStr = word.toLowerCase().split("");
+    for(let i = 0; i < newStr.length; i++) {
+        if(invalidChar.includes(newStr[i])) {
+            newStr.splice(i, 1);
+            i--;
+        }
+    }
+    for(let i = 0; i < newStr.length / 2; i++) {
+        if(newStr[i] !== newStr[newStr.length - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
